ef4c14b9941ce3456e10cdcdded8d8a36a031558
xueqzhan <kenzhang@redhat.com>
2025-01-23 13:37:47 -0500

    Add vsphere variant mapping

--- pkg/components/cloudcompute/vsphereprovider/component.go
+++ pkg/components/cloudcompute/vsphereprovider/component.go
@@ -14,6 +14,7 @@ var VSphereProviderComponent = Component{
 		Name:                 "Cloud Compute / vSphere Provider",
 		Operators:            []string{},
 		DefaultJiraComponent: "Cloud Compute / vSphere Provider",
+		Variants:             []string{"Platform:vsphere"},
 		Matchers:             []config.ComponentMatcher{},
 	},
 }
