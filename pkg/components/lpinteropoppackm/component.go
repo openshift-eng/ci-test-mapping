b33ebb756ee87b9713e84988818cbd8cf3c65334
Forrest Babcock <36795216+neisw@users.noreply.github.com>
2026-08-12 11:38:24 -0400

    Revert "INTEROP-9281: Fix OPP LP Interop SuiteRegEx patterns"

--- pkg/components/lpinteropoppackm/component.go
+++ pkg/components/lpinteropoppackm/component.go
@@ -17,9 +17,7 @@ var LPinteropOPPACMComponent = Component{
 		Operators:            []string{},
 		DefaultJiraComponent: "lp-interop--OPP-ACM",
 		Matchers: []config.ComponentMatcher{
-			{SuiteRegEx: regexp.MustCompile(`^lp-interop--OPP--Observability`)},
-			{SuiteRegEx: regexp.MustCompile(`^acm-opp-app$`)},
-			{SuiteRegEx: regexp.MustCompile(`^lp-interop--OPP--acm`)},
+			{SuiteRegEx: regexp.MustCompile(`^lp-interop--OPP--acm-`)},
 		},
 	},
 }
